ee280c680cacecab91a125ffb846938ae4c664f9
armoar334 <alfiecgordon@gmail.com>
2023-12-08 17:24:45 +0000

    More stuff, blinking

--- main.lua
+++ main.lua
@@ -1,3 +1,12 @@
+--[[
+Notes:
+ - Qbby drops one pixel down when walking
+ - If he is within 5 pixels of a grid border and crouches,
+   he will snap to the grid
+]]--
+
+
+
 function love.load()
 	-- Window setup
 	window_width = 400
@@ -9,10 +18,10 @@ function love.load()
 	-- Game state stuff
 	frame = 1
 
-
 	-- Setup for sprite
 	qbby = love.graphics.newCanvas(24, 30)
-	qbby_eyes_state = "squint"
+	qbby_eyes_state = "normal"
+	qbby_legs_state = "standing"
 end
 
 function love.update()
@@ -45,16 +54,43 @@ function update_qbby_sprite()
 
 	-- Eyes
 	love.graphics.setColor(0, 0, 0, 1)
+	love.graphics.setLineWidth(2)
+	love.graphics.setLineStyle("rough")
+
 	if ( qbby_eyes_state == "squint" )
 	then
-		love.graphics.setLineWidth(2)
-		love.graphics.setLineStyle("rough")
-		love.graphics.line(10,8, 13,11)
-		love.graphics.line(18,10, 21,7)
+		love.graphics.line(9.5,7.5, 13,11)
+		love.graphics.line(17.5,10.5, 21,7) -- have to -/+ 0.5 or there is weird fishtailing at 400x240
 	else
-		love.graphics.rectangle("fill", 10,8, 2,2)
-		love.graphics.rectangle("fill", 18,8, 2,2)
+		eyes_frame = frame % 270
+		-- This sucks but a lookup table sucks more
+		if ( eyes_frame == 1 or eyes_frame == 2 or eyes_frame == 5 or eyes_frame == 6)
+		then
+			-- Blink start/end
+			love.graphics.rectangle("fill", 10,9, 2,1)
+			love.graphics.rectangle("fill", 18,9, 2,1)
+		elseif ( eyes_frame == 3 or eyes_frame == 4)
+		then
+			-- Blink middle
+			love.graphics.rectangle("fill", 9,9, 3,1)
+			love.graphics.rectangle("fill", 17,9, 3,1)
+		else
+			-- No blink
+			love.graphics.rectangle("fill", 10,8, 2,2)
+			love.graphics.rectangle("fill", 18,8, 2,2)
+		end
 	end
+
+	-- Legs
+	-- no animation yet, cba
+	-- 5.5 in
+	if ( qbby_legs_state == "walking" ) -- drops 1 pixel when walking
+	then
+	else
+		love.graphics.line(5.5,24, 5.5, 30)
+		love.graphics.line(18.5,24, 19.5, 30)
+	end
+	
 	-- Switch back
     love.graphics.setCanvas()
 end
